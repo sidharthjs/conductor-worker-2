90e46c3902ce87d62859e57feff951e06e055321
Sidharth J <sidharthj@invisibl.io>
2022-10-29 20:46:21 +0530

    fix: main.go

--- main.go
+++ main.go
@@ -18,12 +18,10 @@ func main() {
 
 	if conductorServer == "" {
 		log.Fatal("conductor server url is invalid")
-		// fmt.Println("conductor server url is invalid")
 	}
 	batchSize, err := strconv.Atoi(batchSizeEnv)
 	if err != nil {
 		log.Fatalf("batch size value invalid: %s", err)
-		// fmt.Printf("batch size value invalid: %s\n", err)
 	}
 
 	apiClient := client.NewAPIClient(
@@ -32,11 +30,9 @@ func main() {
 			conductorServer,
 		),
 	)
-	// fmt.Println("sleeping")
-	// time.Sleep(1 * time.Minute)
 
 	tr := worker.NewTaskRunnerWithApiClient(apiClient)
-	// tr.StartWorker("upload_file", workers.UploadTask, batchSize, 5*time.Second)
+	tr.StartWorker("upload_file", workers.UploadTask, batchSize, 5*time.Second)
 	tr.StartWorker("write_to_db", workers.WriteToDBTask, batchSize, 5*time.Second)
 	log.Info("started workers")
 	tr.WaitWorkers()
